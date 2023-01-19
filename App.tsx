befed44763147eed6418ae818051f895873387b4
agusss03 <agus22dominguez@gmail.com>
2023-01-19 15:26:41 -0300

    I´ve updated some changes

--- App.tsx
+++ App.tsx
@@ -1,6 +1,39 @@
 import React from 'react';
-import {Image, SafeAreaView, StyleSheet, Text, View} from 'react-native';
-import List from './src/components/List';
+import {
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+} from 'react-native';
+import Item from './Cryptos';
+const cryptos = [
+  {
+    id: '1',
+    logo: require('./src/components/images/flecha/bitcoin.jpg'),
+    name: 'Bitcoin',
+    abr: 'BTC',
+    price: 7215.68,
+    status: 1.82,
+  },
+  {
+    id: '2',
+    logo: require('./src/components/images/flecha/ethereum.png'),
+    name: 'Ethereum',
+    abr: 'ETH',
+    price: 146.83,
+    status: 1.46,
+  },
+  {
+    id: '3',
+    logo: require('./src/components/images/flecha/xrp.png'),
+    name: 'XRP',
+    abr: 'XRP',
+    price: 0.220568,
+    status: -2.47,
+  },
+];
 
 const App = () => {
   return (
@@ -17,9 +50,13 @@ const App = () => {
         </View>
         <View style={styles.espacio} />
         <View>
-          <List />
-        </View>
-        <View>
+          <View>
+            <FlatList
+              data={cryptos}
+              keyExtractor={item => item.id}
+              renderItem={({item}) => <Item crypto={item} />}
+            />
+          </View>
           <Text style={styles.btnAddCrypto}> + Add a Cryptocurrency</Text>
         </View>
       </SafeAreaView>
---
+++ Cryptos.tsx
@@ -0,0 +1,110 @@
+import React from 'react';
+import {View, Text, StyleSheet, Image} from 'react-native';
+
+type ItemProps = {
+  crypto: {
+    id: string;
+    name: string;
+    abr: string;
+    price: number;
+    logo: any;
+    status: number;
+  };
+};
+
+const Item = ({crypto}: ItemProps) => (
+  <>
+    <View style={styles.wrapper}>
+      {/*Left side*/}
+      <View style={styles.left}>
+        <Image source={crypto.logo} style={styles.logo} />
+        <View style={styles.titleView}>
+          <Text style={styles.titulo}>{crypto.name}</Text>
+          <Text>{crypto.abr}</Text>
+        </View>
+      </View>
+
+      {/*Right side*/}
+      <View style={styles.derecha}>
+        <Text style={styles.valor}>${crypto.price}</Text>
+        <View style={styles.ganancia}>
+          <Image
+            source={
+              crypto.status > 0
+                ? require('./arrows/FlechaVerde.png')
+                : require('./arrows/FlechaRoja.png')
+            }
+            style={styles.flechaV}
+          />
+          <Text
+            style={
+              crypto.status > 0 ? styles.gananciaNumero : styles.perdidaNumero
+            }>
+            {Math.abs(crypto.status)}%
+          </Text>
+        </View>
+      </View>
+    </View>
+    <View style={styles.separador} />
+  </>
+);
+
+const styles = StyleSheet.create({
+  wrapper: {
+    paddingHorizontal: 16,
+    marginTop: 15,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  logo: {
+    height: 50,
+    width: 50,
+    borderRadius: 50,
+  },
+  left: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  titleView: {
+    marginLeft: 7,
+  },
+  titulo: {
+    fontWeight: 'bold',
+    color: '#000',
+    fontSize: 16,
+  },
+  derecha: {
+    alignItems: 'flex-end',
+  },
+  valor: {
+    fontWeight: 'bold',
+    fontSize: 15,
+    color: '#000',
+  },
+  ganancia: {
+    flexDirection: 'row',
+  },
+  flechaV: {
+    height: 10,
+    width: 10,
+    marginRight: 4,
+    marginTop: 6,
+  },
+  gananciaNumero: {
+    color: '#0C9B4C',
+  },
+  perdidaNumero: {
+    color: '#FF1733',
+  },
+  separador: {
+    borderBottomColor: '#E7E7E7',
+    borderBottomWidth: 1,
+    marginHorizontal: 15,
+    marginTop: 15,
+  },
+});
+
+export default Item;
+
+//<Image source={crypto.status > 0 ? increaseArrow : decreaseArrow} />
